57fbeeb482c19b8a01f09eabab343a1eb888506f
ytodorov <ytodorov@ytodorov.com>
2017-09-02 21:55:35 +0300

    I-O Distribution Key.xlsx PL

--- DimoPdfToExcelSln/UnitTests/ParseDistributionKeysTests.cs
+++ DimoPdfToExcelSln/UnitTests/ParseDistributionKeysTests.cs
@@ -13,11 +13,15 @@ namespace UnitTests
             var rootSolution = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.Parent.Parent.FullName;
             var wwwRootFolder = Path.Combine(rootSolution, "DimoPdfToExcelWeb", "wwwroot");
             Utils.PopulateMappingDictionaries(wwwRootFolder);
-            Assert.NotEmpty(Mappings.HungarianBsRows);
-
+            
             foreach (var row in Mappings.HungarianBsRows)
             {
                 Assert.False(row.GoesToRowNumber == 0);                
+            }
+
+            foreach (var row in Mappings.HungarianPlRows)
+            {
+                Assert.False(row.GoesToRowNumber == 0);
             }
 
         }
