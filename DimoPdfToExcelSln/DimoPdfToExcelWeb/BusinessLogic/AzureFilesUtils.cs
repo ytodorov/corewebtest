fd16348c07b2a3b99a099831554c83c51ea7a669
ytodorov <ytodorov@ytodorov.com>
2017-09-08 22:54:35 +0300

    az2

---
+++ DimoPdfToExcelSln/DimoPdfToExcelWeb/BusinessLogic/AzureFilesUtils.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.File;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DimoPdfToExcelWeb.BusinessLogic
+{
+    public class AzureFilesUtils
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        //[EditorBrowsable(EditorBrowsableState.Never)]
+        //public static override bool Equals(object obj)
+        //{
+        //    throw new Exception("Assertion does not implement Equals, use Ensure or Require");
+        //}
+
+        //[EditorBrowsable(EditorBrowsableState.Never)]
+        //public static new bool ReferenceEquals(object objA, object objB)
+        //{
+        //    throw new Exception("Assertion does not implement ReferenceEquals, use Ensure or Require");
+        //}
+
+        public static CloudFileDirectory GetCloudDirectoryShare(bool inputFilesShare = true)
+        {
+            StorageCredentials sc = new StorageCredentials("yordansto" + "rageaccount",
+               "WHN5k4wFTmFmiuzFaWkB4N646yYE9PjrOpiyx7j5iWe3XC" + "GVgi/5ja8jT9LGiIXsvaLB9DYDpUenu7/NQJVZWA==");
+            CloudStorageAccount cloudStorageAccount = new CloudStorageAccount(sc, true);
+
+            // Create a CloudFileClient object for credentialed access to Azure File storage.
+            CloudFileClient fileClient = cloudStorageAccount.CreateCloudFileClient();
+
+            // Get a reference to the file share we created previously.
+            CloudFileShare share = fileClient.GetShareReference("dimo");
+            var boolResult = share.CreateIfNotExistsAsync().Result;
+            CloudFileDirectory rootDir = share.GetRootDirectoryReference();
+            CloudFileDirectory resultDir = null;
+            if (inputFilesShare)
+            {
+                resultDir = rootDir.GetDirectoryReference("DimoInputPdfFiles");
+            }
+            else
+            {
+                resultDir = rootDir.GetDirectoryReference("DimoOutputExcelFiles");
+            }
+            boolResult = resultDir.CreateIfNotExistsAsync().Result;
+            return resultDir;
+        }
+
+        public static void UploadFile(string directoryName, string fileNameWithExtension, Stream stream)
+        {
+            CloudFileDirectory cloudFileDirectory = GetCloudDirectoryShare();
+
+            CloudFile cloudFileShare = cloudFileDirectory.GetFileReference(fileNameWithExtension);
+            var boolResult = cloudFileShare.DeleteIfExistsAsync().Result;
+            cloudFileShare.UploadFromStreamAsync(stream).Wait();
+            
+        }
+
+        public static void UploadFile(string directoryName, string fileNameWithExtension, string path)
+        {
+            CloudFileDirectory cloudFileDirectory = GetCloudDirectoryShare();
+
+            CloudFile cloudFileShare = cloudFileDirectory.GetFileReference(fileNameWithExtension);
+            var boolResult = cloudFileShare.DeleteIfExistsAsync().Result;
+            cloudFileShare.UploadFromFileAsync(path).Wait();
+        }
+
+        public static void DeleteFile(string directoryName, string fileNameWithExtension, string path)
+        {
+            CloudFileDirectory cloudFileDirectory = GetCloudDirectoryShare();
+
+            CloudFile cloudFileShare = cloudFileDirectory.GetFileReference(fileNameWithExtension);
+            var boolResult = cloudFileShare.DeleteIfExistsAsync().Result;
+        }
+
+        public static List<CloudFile> ListAllFiles()
+        {
+            CloudFileDirectory cloudFileDirectory = GetCloudDirectoryShare();
+            FileContinuationToken fct = new FileContinuationToken();
+
+            FileResultSegment fileResultSegment = cloudFileDirectory.ListFilesAndDirectoriesSegmentedAsync(fct).Result;
+
+            List<IListFileItem> list = fileResultSegment.Results.ToList();
+            List<CloudFile> cloudFileList = new List<CloudFile>();
+            foreach (var l in list)
+            {
+                CloudFile cf = l as CloudFile;
+                if (cf != null)
+                {
+                    cloudFileList.Add(cf);
+                }                
+            }
+            return cloudFileList;
+
+
+        }
+
+
+
+
+    }
+}
--- DimoPdfToExcelSln/UnitTests/AzureFilesTests.cs
+++ DimoPdfToExcelSln/UnitTests/AzureFilesTests.cs
@@ -1,54 +1,33 @@
-﻿using Microsoft.WindowsAzure.Storage;
+﻿using DimoPdfToExcelWeb.BusinessLogic;
+using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.File;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using Xunit;
 
 namespace UnitTests
 {
-    public class AzureFilesTests
+    public class AzureFilesTests : UnitTestBase
     {
         [Fact]
-        public void UploadFileTest()
+        public void UploadFileFromPathTest()
         {
-            StorageCredentials sc = new StorageCredentials("yordansto" + "rageaccount",
-                "WHN5k4wFTmFmiuzFaWkB4N646yYE9PjrOpiyx7j5iWe3XC" + "GVgi/5ja8jT9LGiIXsvaLB9DYDpUenu7/NQJVZWA==");
-            CloudStorageAccount cloudStorageAccount = new CloudStorageAccount(sc, true);
-
-            // Create a CloudFileClient object for credentialed access to Azure File storage.
-            CloudFileClient fileClient = cloudStorageAccount.CreateCloudFileClient();
-
-            // Get a reference to the file share we created previously.
-            CloudFileShare share = fileClient.GetShareReference("test");
-            var res = share.CreateIfNotExistsAsync().Result;
-
-            
-
-            // Ensure that the share exists.
-            if (share.ExistsAsync().Result)
+            foreach (var path in CroatiaFileNames)
             {
-                //// Get a reference to the root directory for the share.
-                //CloudFileDirectory rootDir = share.GetRootDirectoryReference();
-
-                //// Get a reference to the directory we created previously.
-                //CloudFileDirectory sampleDir = rootDir.GetDirectoryReference("CustomLogs");
-
-                //// Ensure that the directory exists.
-                //if (sampleDir.ExistsAsync().Result)
-                //{
-                //    // Get a reference to the file we created previously.
-                //    CloudFile file = sampleDir.GetFileReference("Log1.txt");
-
-                //    // Ensure that the file exists.
-                //    if (file.ExistsAsync().Result)
-                //    {
-                //        // Write the contents of the file to the console window.
-                //        Console.WriteLine(file.DownloadTextAsync().Result);
-                //    }
-                //}
+                var fileNameWithExtension = Path.GetFileName(path);
+                AzureFilesUtils.UploadFile("testDir", fileNameWithExtension, path);
             }
         }
+
+        [Fact]
+        public void ListFilesTest()
+        {
+            var list = AzureFilesUtils.ListAllFiles();
+            Assert.NotEmpty(list);
+        }
+
     }
 }
