180c64df5c0467e25938a51935e70a68ede1fa0d
ytodorov <ytodorov@ytodorov.com>
2017-10-01 21:27:02 +0300

    set time

--- DimoPdfToExcelSln/DimoPdfToExcelWeb/wwwroot/js/site.js
+++ DimoPdfToExcelSln/DimoPdfToExcelWeb/wwwroot/js/site.js
@@ -1,1 +1,11 @@
 ﻿// Write your JavaScript code.
+setCorrectTime();
+setInterval(setCorrectTime, 1000);
+
+function setCorrectTime()
+{
+    var span = $("#spanTime");
+    var time = new Date();
+    span.text(time.toLocaleString());
+}
+
