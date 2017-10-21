f5161da5180030e9c89155c61ca76ea1006062b3
ytodorov <ytodorov@ytodorov.com>
2017-10-21 08:15:23 +0300

    home page

--- DimoPdfToExcelSln/DimoPdfToExcelWeb/Startup.cs
+++ DimoPdfToExcelSln/DimoPdfToExcelWeb/Startup.cs
@@ -32,82 +32,6 @@ namespace DimoPdfToExcelWeb
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
-
-            // Add application services.
-            services.AddTransient<IEmailSender, EmailSender>();
-
-            services.AddAuthentication().AddGoogle(googleOptions =>
-            {
-                googleOptions.ClientId = "327729092284-8umlocf6nrn9nn7viu8fm9b8dlmiu3jf.apps.googleusercontent.com";
-                googleOptions.ClientSecret = "k7fbt8DFAgLKIRDZ7yei4yhx";
-            });
-
-            services.AddMvc()
-                   .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver()); ;
-
-            // Adds a default in-memory implementation of IDistributedCache.
-            services.AddDistributedMemoryCache();
-
-            services.AddSession(options =>
-            {
-                // Set a short timeout for easy testing.
-                options.IdleTimeout = TimeSpan.FromSeconds(10);
-                options.Cookie.HttpOnly = true;
-            });
-
-            services.AddWebMarkupMin(
-        options =>
-        {
-            options.AllowMinificationInDevelopmentEnvironment = true;
-            options.AllowCompressionInDevelopmentEnvironment = true;
-
-            
-        })
-        .AddHtmlMinification(
-            options =>
-            {
-                options.MinificationSettings.RemoveRedundantAttributes = true;
-                options.MinificationSettings.RemoveHttpProtocolFromAttributes = true;
-                options.MinificationSettings.RemoveHttpsProtocolFromAttributes = true;
-
-                options.CssMinifierFactory = new MsAjaxCssMinifierFactory();
-                options.JsMinifierFactory = new MsAjaxJsMinifierFactory();
-            })
-        .AddHttpCompression();
-
-            services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
-            services.AddResponseCompression(options =>
-            {
-                options.MimeTypes = new[]
-                {
-            // Default
-            "text/plain",
-            "text/css",
-            "application/javascript",
-            "text/html",
-            "application/xml",
-            "text/xml",
-            "application/json",
-            "text/json",
-            // Custom
-            "image/svg+xml",
-            "application/font-woff2"
-                };
-            });
-
-            // Add Kendo UI services to the services container
-            services.AddKendo();
-        }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -148,15 +72,86 @@ namespace DimoPdfToExcelWeb
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
-
-
             // Configure Kendo UI
             app.UseKendo(env);
 
             Utils.PopulateHungarianMappingDictionaries(env.WebRootPath);
             Utils.PopulateSerbianMappingDictionaries(env.WebRootPath);
             Utils.PopulateCroatianMappingDictionaries(env.WebRootPath);
+        }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddAuthentication().AddGoogle(googleOptions =>
+            {
+                googleOptions.ClientId = "327729092284-8umlocf6nrn9nn7viu8fm9b8dlmiu3jf.apps.googleusercontent.com";
+                googleOptions.ClientSecret = "k7fbt8DFAgLKIRDZ7yei4yhx";
+            });
+
+            services.AddMvc()
+                   .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver()); ;
+
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+            });
+
+            services.AddWebMarkupMin(
+        options =>
+        {
+            options.AllowMinificationInDevelopmentEnvironment = true;
+            options.AllowCompressionInDevelopmentEnvironment = true;
+        })
+        .AddHtmlMinification(
+            options =>
+            {
+                options.MinificationSettings.RemoveRedundantAttributes = true;
+                options.MinificationSettings.RemoveHttpProtocolFromAttributes = true;
+                options.MinificationSettings.RemoveHttpsProtocolFromAttributes = true;
+
+                options.CssMinifierFactory = new MsAjaxCssMinifierFactory();
+                options.JsMinifierFactory = new MsAjaxJsMinifierFactory();
+            })
+        .AddHttpCompression();
+
+            services.Configure<GzipCompressionProviderOptions>(options => options.Level = System.IO.Compression.CompressionLevel.Optimal);
+            services.AddResponseCompression(options =>
+            {
+                options.MimeTypes = new[]
+                {
+            // Default
+            "text/plain",
+            "text/css",
+            "application/javascript",
+            "text/html",
+            "application/xml",
+            "text/xml",
+            "application/json",
+            "text/json",
+            // Custom
+            "image/svg+xml",
+            "application/font-woff2"
+                };
+            });
+
+            // Add Kendo UI services to the services container
+            services.AddKendo();
         }
     }
 }
