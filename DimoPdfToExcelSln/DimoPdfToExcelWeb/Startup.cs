900b6d8d8460690aaec1a54b92dce8d1c4de6e17
ytodorov <ytodorov@ytodorov.com>
2017-10-23 09:42:07 +0300

    url rewrite !!! added web.config

--- DimoPdfToExcelSln/DimoPdfToExcelWeb/Startup.cs
+++ DimoPdfToExcelSln/DimoPdfToExcelWeb/Startup.cs
@@ -1,25 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DimoPdfToExcelWeb.BusinessLogic;
+using DimoPdfToExcelWeb.Data;
+using DimoPdfToExcelWeb.Models;
+using DimoPdfToExcelWeb.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Net.Http.Headers;
-using Microsoft.AspNetCore.ResponseCompression;
-using System.IO;
-using OfficeOpenXml;
-using DimoPdfToExcelWeb.BusinessLogic;
 using Newtonsoft.Json.Serialization;
-using DimoPdfToExcelWeb.Models;
-using Microsoft.AspNetCore.Identity;
-using DimoPdfToExcelWeb.Data;
-using Microsoft.EntityFrameworkCore;
-using DimoPdfToExcelWeb.Services;
+using System;
 using WebMarkupMin.AspNetCore2;
 using WebMarkupMin.MsAjax;
-using WebMarkupMin.Yui;
 
 namespace DimoPdfToExcelWeb
 {
@@ -65,12 +61,25 @@ namespace DimoPdfToExcelWeb
 
             app.UseWebMarkupMin();
 
-            app.UseMvc(routes =>
+            RewriteOptions ro = new RewriteOptions();
+
+            var cd = Environment.MachineName;
+
+            if (!cd.ToUpperInvariant().Contains("YORDAN".ToUpperInvariant()))
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+                ro.AddRedirectToHttps();
+            }
+
+            ro.AddIISUrlRewrite(env.ContentRootFileProvider, "web.config");
+
+            app.UseRewriter(ro);
+
+            app.UseMvc(routes =>
+           {
+               routes.MapRoute(
+                   name: "default",
+                   template: "{controller=Home}/{action=Index}/{id?}");
+           });
 
             // Configure Kendo UI
             app.UseKendo(env);
@@ -98,6 +107,11 @@ namespace DimoPdfToExcelWeb
                 googleOptions.ClientId = "327729092284-8umlocf6nrn9nn7viu8fm9b8dlmiu3jf.apps.googleusercontent.com";
                 googleOptions.ClientSecret = "k7fbt8DFAgLKIRDZ7yei4yhx";
             });
+
+            //services.Configure<MvcOptions>(options =>
+            //{
+            //    options.Filters.Add(new RequireHttpsAttribute());
+            //});
 
             services.AddMvc()
                    .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver()); ;
